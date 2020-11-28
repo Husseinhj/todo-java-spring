76b729c2c068a9a90190669c6b9ee9a314cdb665
Hussein Habibi Juybari <hussein.juybari@gmail.com>
2020-11-28 15:56:08 +0330

    Add im memory security role

--- src/main/java/io/husseinhj/todo/todo/security/ApplicationSecurityConfig.java
+++ src/main/java/io/husseinhj/todo/todo/security/ApplicationSecurityConfig.java
@@ -1,13 +1,26 @@
 package io.husseinhj.todo.todo.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -18,5 +31,18 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .httpBasic();
+
+        http.csrf().disable();
+    }
+
+    @Override
+    protected UserDetailsService userDetailsService() {
+        UserDetails husseinUser = User.builder()
+                .username("husseinhj")
+                .password(this.passwordEncoder.encode("password"))
+                .roles("ADMIN") // ROLE_ADMIN
+                .build();
+
+        return new InMemoryUserDetailsManager(husseinUser);
     }
 }
